85de83ffa78141b12bcdba4fe6233e8aae34ff66
LEEHAKMIN <kia54569@gmail.com>
2024-07-12 11:12:27 +0900

    2024.07.12 코딩테스트 연습 커밋

---
+++ Algorithm2/src/week3/permutation/Solution.java
@@ -0,0 +1,16 @@
+package week3.permutation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        return result;
+    }
+    // 순열을 만들어서 result에 추가하는 코드
+    void backtrack(){
+
+    }
+}
