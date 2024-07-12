974049042917bed24653be2c8c580214315d151a
LEEHAKMIN <kia54569@gmail.com>
2024-07-12 14:42:00 +0900

    2024.07.12 코딩테스트 콤비네이션 커밋

--- Algorithm2/src/week3/permutation/Solution.java
+++ Algorithm2/src/week3/permutation/Solution.java
@@ -6,11 +6,39 @@ import java.util.List;
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-
+        boolean[] visited = new boolean[nums.length];
+        backtrack(new ArrayList<>(),nums, visited,result);
         return result;
     }
     // 순열을 만들어서 result에 추가하는 코드
-    void backtrack(){
+    void backtrack(List<Integer>curr ,int[] nums, boolean[]visited, List<List<Integer>> result){
+        //basecase
+        if(curr.size() == nums.length){
+            result.add(new ArrayList<>(curr));
+            return;
+        }
 
+
+        //recursive call
+        for(int i = 0; i < nums.length; i++){
+            if (visited[i]) continue;
+
+            curr.add(nums[i]);//add
+            visited[i] = true;
+            backtrack(curr,nums,visited, result);
+            curr.remove(curr.size()-1); //remove
+            visited[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 3, 4};
+        List<List<Integer>> permutations = solution.permute(nums);
+
+        for (List<Integer> permutation : permutations) {
+            System.out.println(permutation);
+        }
     }
 }
+
